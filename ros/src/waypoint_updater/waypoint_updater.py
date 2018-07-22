78cf8458cd0ce3770a132d542b13ea9f05c9c6cb
Saimir Baci <saimirbaci@hotmail.com>
2018-07-22 21:12:47 +0200

    Modified the topic subscription

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,7 +33,10 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        rospy.Subscriber('/trafficwaypoints', Int32, self.traffic_cb)
+        #rospy.Subscriber('/trafficwaypoints', Int32, self.traffic_cb)
+
+        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        #self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
